42deef28d86971d7e33a730315fe1cb6a0c76475
mc0re <mnikitin0@gmail.com>
2023-05-27 17:02:16 +0200

    Added Blazor

--- SearchLib/WordSearcher.cs
+++ SearchLib/WordSearcher.cs
@@ -6,24 +6,30 @@ namespace SearchLib;
 
 public class WordSearcher
 {
+    private string DataPath =>
+        Path.Combine(
+            Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? "",
+            "Data");
+
+
     public IReadOnlyCollection<string> GetDictionaries()
     {
-        return Directory.GetFiles("Data").Select(f => Path.GetFileNameWithoutExtension(f)).ToList();
+        return Directory.GetFiles(DataPath).Select(f => Path.GetFileNameWithoutExtension(f)).ToList();
     }
 
 
     /// <summary>
     /// Search for a word according to the pattern.
     /// </summary>
-    /// <param name="language"></param>
-    /// <param name="minLength"></param>
-    /// <param name="maxLength"></param>
-    /// <param name="pattern"></param>
-    /// <returns></returns>
+    /// <param name="language">One of the available languages (see <see cref="GetDictionaries"/>)</param>
+    /// <param name="minLength">Minimum word length</param>
+    /// <param name="maxLength">Maximum word length, 0 means no limit</param>
+    /// <param name="pattern">Pattern to search</param>
+    /// <returns>A collection of found words</returns>
     public IReadOnlyCollection<string> Search(string language, uint minLength, uint maxLength, string? pattern)
     {
         var res = new List<string>();
-        var words = File.ReadAllLines($"Data/{language}.txt", Encoding.Latin1).Where(w => !string.IsNullOrWhiteSpace(w)).ToList();
+        var words = File.ReadAllLines($"{DataPath}/{language}.txt", Encoding.Latin1).Where(w => !string.IsNullOrWhiteSpace(w)).ToList();
 
         var rx = MaskToRegex(pattern ?? ".+");
 
